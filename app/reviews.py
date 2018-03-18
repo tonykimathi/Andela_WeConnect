a7028aa12575f6cad467ea407626f9d911ca71d1
Tony-Kimathi <kimathitony93@gmail.com>
2018-03-19 00:30:37 +0300

    [chore #156071703] Add docstrings to describe program logic

--- app/reviews.py
+++ app/reviews.py
@@ -2,13 +2,26 @@ from app.data import Data
 
 
 class Reviews():
-    review_id = len(Data.reviews_data) + 1
 
     def __init__(self):
+
+        """
+            Business object initializer.
+        """
+
         self.review_dict = {}
         self.review_id = len(Data.reviews_data) + 1
 
     def create_review(self, businessId, review_name, body):
+
+        """
+            Creates Review objects.
+            Arguments:
+                Business ID: A unique identifier for the business being reviewed.
+                Review Name: A unique identify for the Review
+                Body: Information containing the actual review.
+        """
+
         for review in Data.reviews_data:
             if businessId == review['businessId']:
                 self.review_dict['businessId'] = businessId
@@ -23,6 +36,13 @@ class Reviews():
 
     @staticmethod
     def get_all_reviews(businessId):
+
+        """
+            Views all reviews about a particular business.
+            Arguments:
+                Business ID: A unique identifier for the business.
+        """
+        
         for review in Data.reviews_data:
             if businessId == review['businessId']:
                 return {"all_reviews": Data.reviews_data}
